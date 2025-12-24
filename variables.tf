038cdfcd2d59b9f5b798bd47dee98889ba03e286
Kiran <knune@hyperlocal.us>
2025-12-24 15:56:51 +0530

    intial comment

---
+++ variables.tf
@@ -0,0 +1,22 @@
+#Variables
+variable "rg_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "nsg_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  default = "insecurestorage123"
+}
+
+
